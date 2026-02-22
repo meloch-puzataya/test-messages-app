75deeb1ebb7613d7f5789693934cc82fe4b77065
meloch-puzataya <meloch.puzataya3@gmail.com>
2026-02-22 21:34:30 +0100

    feat: add add_message endpoint

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -11,6 +11,11 @@ struct Message {
     text: String,
 }
 
+#[derive(Deserialize)]
+struct NewMessage {
+    text: String,
+}
+
 #[get("/messages")]
 async fn get_messages(db_pool: actix_web::web::Data<sqlx::PgPool>) -> impl Responder {
     let rows = sqlx::query_as::<_, Message>("SELECT id, text FROM messages")
@@ -25,6 +30,28 @@ async fn get_messages(db_pool: actix_web::web::Data<sqlx::PgPool>) -> impl Respo
         }
     }
 }
+
+#[post("/message")]
+async fn add_message(
+    db_pool: web::Data<sqlx::PgPool>,
+    new_message: web::Json<NewMessage>,
+) -> impl Responder {
+    let query = sqlx::query_as::<_, Message>(
+        "INSERT INTO messages (text) VALUES ($1) RETURNING id, text",
+    )
+    .bind(&new_message.text)
+    .fetch_one(db_pool.get_ref())
+    .await;
+
+    match query {
+        Ok(message) => HttpResponse::Ok().json(message),
+        Err(err) => {
+            eprintln!("Database insert error: {:?}", err);
+            HttpResponse::InternalServerError().body("Error adding message")
+        }
+    }
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -44,6 +71,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Cors::permissive())
             .app_data(actix_web::web::Data::new(pool.clone()))
             .service(get_messages)
+            .service(add_message)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
